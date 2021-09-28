e4b2e9ad465fbe6f549693b546898effec2090db
Neusi75 <72435255+Neusi75@users.noreply.github.com>
2021-09-28 12:44:56 +0200

    Crida API amb fetch

--- Nivell 3/app.js
+++ Nivell 3/app.js
@@ -1,43 +1,36 @@
 
-
 $(document).ready(function () {
-
   
   const apiUrl = "http://api.icndb.com/jokes/random";
   valores(); 
 
   function valores(){
-    $.get((URL = apiUrl), function(data){
-    let dataValue = data.value;
-    let joke = dataValue.joke;
-    document.querySelector("#texto").innerHTML = joke;
-
-    });
+    fetch(apiUrl,{
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'same-origin', 
+      cache: 'no-cache',
+      referrerPolicy: 'no-referrer'
+     })
+      .then( response => {
+        console.log('response =', response)
+          return response.json(); 
+      })
+      .then(data => {
+        console.log('data = ', data);
+        $("#texto").html(data.value.joke)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+ 
 };
 
      $("#btnClick").click(function(element){
         element.preventDefault();
         valores();
+     
            
      });
-
-    fetch(apiUrl,{
-    method: 'POST',
-    mode: 'cors',
-    credentials: 'same-origin', 
-    cache: 'no-cache',
-    referrerPolicy: 'no-referrer'
-})
-    .then( response => {
-      console.log('response =', response)
-        return response.json(); 
-    })
-    .then(data => {
-      console.log('data = ', data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
- 
 
 });
